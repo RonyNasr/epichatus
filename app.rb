47c57632ffb5f68d96d9d090b709ca5f1422c032
Liam Ellis <liamhellis@gmail.com>
2016-07-25 16:47:29 -0700

    somehting

--- app.rb
+++ app.rb
@@ -1,3 +1,32 @@
 require("bundler/setup")
 Bundler.require(:default)
 Dir[File.dirname(__FILE__) + '/lib/*.rb'].each { |file| require file }
+include BCrypt
+
+get '/' do
+  erb(:index)
+end
+
+post '/signup/?' do
+  if User.find_by email: params['signup_email']
+    'That email is already registered to an account'
+    redirect '/ '
+  elsif User.find_by username: params['signup_username']
+    'That username is taken'
+    redirect '/'
+  else
+    secure_password = Password.create(params['signup_password'])
+    @user = User.create({username: params['signup_username'], email: params['signup_email'], password: secure_password})
+    erb(:user)
+  end
+end
+
+post '/login/?' do
+  if user = User.authenticate(params)
+    session[:user] = user
+    redirect_to_original_request
+  else
+    flash[:notice] = "You could not be signed in. Please re-enter you email and password"
+    redirect '/signin'
+  end
+end
