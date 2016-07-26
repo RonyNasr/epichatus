5214359932abac93ca1d58b6da2d327ecfb95615
Liam Ellis <liamhellis@gmail.com>
2016-07-25 21:47:44 -0700

    login route up with css redesign

--- public/js/main.js
+++ public/js/main.js
@@ -1,3 +1,13 @@
 $(document).ready(function(){
+  console.log('What up');
+  var $login = $('#login');
+  var $signin = $('#signin');
+
+  $login.click(function(){
+    $('.login').fadeIn(1000);
+  });
+  $signin.click(function(){
+    $('.signin').fadeIn(1000);
+  });
 
 });
