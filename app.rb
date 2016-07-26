318fd6cb8f71fa4e07dbf533dbc0df4881b29971
Katarina Tuttle and Rony Nasr <katarina+rony@>
2016-07-26 16:39:47 -0700

    added classes and activated faye

--- app.rb
+++ app.rb
@@ -2,6 +2,17 @@ require("bundler/setup")
 Bundler.require(:default)
 Dir[File.dirname(__FILE__) + '/lib/*.rb'].each { |file| require file }
 
+helpers do
+  def faye_path
+    "#{request.scheme}://#{request.host}:9292/faye"
+  end
+
+  def faye_js_path
+    faye_path + ".js"
+  end
+end
+
 get('/') do
-  erb(:index)
+  @users = User.all()
+  erb(:"index.html")
 end
