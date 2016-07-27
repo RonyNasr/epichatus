933699bf532fb08940af1e8d86973507b555593c
Katarina Tuttle and Rony Nasr <katarina+rony@>
2016-07-27 15:33:55 -0700

    show names of people that user is chatting with

--- app.rb
+++ app.rb
@@ -22,6 +22,7 @@ end
 
 get('/') do
   @users = User.all()
+
   erb(:index)
 end
 
@@ -29,9 +30,12 @@ get '/user' do
   if User.find_by_id(session[:id])
     @user = User.find(session[:id])
     @users = User.all()
+    @conversations = @user.conversations()
+
     erb(:user)
   else
     flash[:notice] = "You have been signed out due to inactivity"
+
     redirect '/'
   end
 end
@@ -39,14 +43,17 @@ end
 post '/signup/?' do
   if User.find_by email: params['signup_email']
     flash[:notice] = "That email is already registered to an account"
+
     redirect '/'
   elsif User.find_by username: params['signup_username']
     flash[:notice] = 'That username is taken'
+
     redirect '/'
   else
     secure_password = Password.create(params['signup_password'])
     @user = User.create({username: params['signup_username'], email: params['signup_email'], password: secure_password})
     session[:id] = @user.id
+
     redirect '/user'
   end
 end
@@ -55,9 +62,11 @@ post '/login/?' do
   if user = User.authenticate(params)
     @user = user
     session[:id] = @user.id
+
     redirect '/user'
   else
     flash[:notice] = "Invalid username or password."
+
     redirect '/'
   end
 end
@@ -65,14 +74,12 @@ end
 post '/logout/?' do
   session.clear
   flash[:notice] = "You have succesfully signed out"
+
   redirect '/'
 end
 
 post '/conversation' do
   @conversation = Conversation.create({sender_id: params['data-sid'], recipient_id: params['data-rid']})
-  @user = User.find(session[:id])
-  @user.conversations.push(@conversation)
-  @other_user = User.find(params['data-rid'])
-  @other_user.conversations.push(@conversation)
+
   redirect '/user'
 end
