abc14e6bb4ffc028f64320e3c9c7cff7042f6d16
sprokuski@gmail.com <Guest@epicodus-35.local>
2016-07-28 11:34:32 -0700

    add passing integration spec

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -10,3 +10,18 @@ set(:show_exceptions, false)
 require('./app')
 
 Dir[File.dirname(__FILE__) + '/../lib/*.rb'].each { |file| require file }
+
+
+RSpec.configure do |config|
+  config.after(:each) do
+    Conversation.all().each() do |conversation|
+      conversation.destroy()
+    end
+    Message.all().each() do |message|
+      message.destroy()
+    end
+    User.all().each() do |user|
+      user.destroy()
+    end
+  end
+end
